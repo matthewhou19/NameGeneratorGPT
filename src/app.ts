1dfae47c9eb589b965e977e3579930148f3f4f6d
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2023-05-22 17:34:49 -0700

    communication succesfully

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
+import { Configuration, OpenAIApi } from "openai";
+export const configuration = new Configuration({
+  apiKey: "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",
+});
+export const openai = new OpenAIApi(configuration);
+
 (document.querySelector(".bannerBlock") as HTMLElement).style.height = "100px";
 
 const submitBt = document.querySelector("#submitNameQuery");
@@ -11,8 +17,38 @@ class NameQeury {
 
 const submitBtnHandeler = function (event: Event) {
   event.preventDefault();
+  console.log("aaa");
   const nameQeury = new NameQeury("female");
-  const names = generateNames(nameQeury);
+
+  generateNames(nameQeury);
 };
 
-function generateNames(nameQuery: NameQeury): string[] {}
+function generatePrompt(nameQuery: NameQeury) {
+  return `Suggest three names for newborn child.
+  the child gender is ${nameQuery}.`;
+}
+
+export default async function generateNames(
+  nameQuery: NameQeury
+): Promise<void | string[]> {
+  if (!configuration.apiKey) {
+    alert(
+      "OpenAI API key not configured, please follow instructions in README.md"
+    );
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(nameQuery),
+      temperature: 0.6,
+    })!;
+    //const data = await JSON.parse(completion.data.choices[0].text!);
+    console.log(completion.data.choices[0].text!);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+submitBt!.addEventListener("click", submitBtnHandeler);
